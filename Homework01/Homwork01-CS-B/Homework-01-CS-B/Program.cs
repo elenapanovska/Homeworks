bfb57bc768d15ab669019243e069088b3ae110c5
Elena <elenapanovska@hotmail.com>
2020-03-03 15:25:14 +0100

    Homework-01-class01-finished

---
+++ Homework01/Homwork01-CS-B/Homework-01-CS-B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework_01_CS_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trafic light....\n");
+            Thread.Sleep(2000);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red light- STOP!\n");
+            Thread.Sleep(2000);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow light - PREPARE.\n");
+            Thread.Sleep(3000);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green light - GO!");
+            Thread.Sleep(3000);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
