76e2a645a8baaa2500db8513a31a10bd800b7f9f
elenapanovska <60360942+elenapanovska@users.noreply.github.com>
2020-03-13 14:28:47 +0100

    Add files via upload

---
+++ Class03-Homework03/StudnetGroup/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StudnetGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = new string[] { "Tomce", "Marko", "Darko", "Mine", "Sara" };
+
+
+            string userInt = Console.ReadLine();
+            if (!int.TryParse(userInt, out int groupNum))
+            {
+                Console.WriteLine("Invalid input");
+            };
+
+            if(groupNum == 1)
+            {
+                Console.WriteLine("The studnets in G1 are:");
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+            } else
+            {
+                Console.WriteLine("The studnets in G2 are:");
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+            };
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Class03-Homework03/Task01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[] sumEvenNum = new int[6];
+            int result = 0;
+            Console.WriteLine("Enter 6 numbers");
+            for (int i = 0; i < sumEvenNum.Length; i++)
+            {
+                string inputNum = Console.ReadLine();
+                if (!int.TryParse(inputNum, out int parsedNum))
+                {
+                    Console.WriteLine("Invalid number");
+
+                };
+
+                sumEvenNum[i] = parsedNum;
+
+                if (sumEvenNum[i] % 2 == 0)
+                {
+                    result = result + sumEvenNum[i];
+                };
+                
+
+            };
+
+            Console.WriteLine($"The result is: {result}");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
