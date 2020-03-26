1fa6e1704de4bfc8047416c2bfe1aa1c6626f707
elenapanovska <60360942+elenapanovska@users.noreply.github.com>
2020-03-26 13:05:56 +0100

    Add files via upload

---
+++ Class04.Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BonusTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birth date ");
+
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            AgeCalculator(birthDate);
+
+            Console.ReadLine();
+        }
+
+        public static int AgeCalculator(DateTime birthdayDate)
+        {
+            int age = DateTime.Today.Year - birthdayDate.Year;
+
+            if ((birthdayDate.Month > DateTime.Now.Month) || (birthdayDate.Month == DateTime.Now.Month && birthdayDate.Day > DateTime.Now.Day))
+            {
+                age--;
+            }
+
+            Console.WriteLine(age);
+            return age;
+        }
+    }
+
+}
