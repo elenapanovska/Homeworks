b0d123cca485ff6bc02991dbc21a6bd626fa3297
Elena <elenapanovska@hotmail.com>
2020-03-26 13:04:00 +0100

    Bonus task done

--- Class04.Homework/BonusTask/Program.cs
+++ Class04.Homework/BonusTask/Program.cs
@@ -6,7 +6,7 @@ namespace BonusTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your birth date ");
 
             DateTime birthDate = DateTime.Parse(Console.ReadLine());
             AgeCalculator(birthDate);
